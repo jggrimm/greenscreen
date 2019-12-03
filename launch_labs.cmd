448472ba0948f7122228b1591b4c7c17720a25cb
Sabina Chen <sabinach@mit.edu>
2019-12-03 00:47:40 -0500

    Update and rename git_pull.cmd to launch_labs.cmd

--- launch_labs.cmd
+++ launch_labs.cmd
@@ -2,12 +2,6 @@ call C:\Users\BWSI-STUDENT\Anaconda2\Scripts\activate.bat
 
 cd \Users\BWSI-STUDENT\Desktop\greenscreen
 
-git clean -fdx
-
-git reset --hard
-
-git pull
-
 call conda activate RMS
 
 call jupyter notebook
